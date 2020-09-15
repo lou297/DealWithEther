e9414841378f818589e27dab8defb9f77f137816
Kim Donghyun <eastmon94@naver.com>
2020-09-15 09:43:14 +0900

    add vuetify / S03P22A103-19

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <v-app id="app">
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
     <router-view />
-  </div>
+  </v-app>
 </template>
 
 <style>
