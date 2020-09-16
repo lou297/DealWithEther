357c5c42b9774d7767f23855e7d01d2c27200c0f
yeonhee <dhfbsk1@naver.com>
2020-09-16 17:18:15 +0900

    modify test

--- frontend/src/components/explorer/test.vue
+++ frontend/src/components/explorer/test.vue
@@ -205,7 +205,6 @@ export default {
             privateKey: "",
             userId: this.$store.state.user.id,
             isCreating: false,
-            // imgSrc: `${require("C:\\Users\\multicampus\\Downloads\\_.png")}`,
         };
     },
     computed: {
---
+++ frontend/src/views/Sell.vue
@@ -0,0 +1,199 @@
+<template>
+    <div>
+        <h-breadcrumb title="상품 등록" description="새로운 상품을 등록합니다."></h-breadcrumb>
+        <v-container>
+            <div row justify-space-around class="backImg">
+                <v-layout row wrap>
+                    <v-flex xl6 lg6 md12 sm12 xs12>
+                        <img :src="item.image"><br>
+                        <div class="form-group">
+                            <input id="upload" type="file" class="form-control" style="height: auto;" @change="onFileChange" />
+                        </div>
+                    </v-flex>
+                    <v-flex xl6 lg6 md12 sm12 xs12>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 mx-auto">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                                <label id="name">상품 이름</label>
+                                                <input type="text" class="form-control" id="name" v-model="item.name" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label id="name">카테고리</label>
+                                                <select class="form-control" id="category" v-model="item.category">
+                                                    <option value="D">디지털/가전</option>
+                                                    <option value="C">유아물품</option>
+                                                    <option value="H">게임/취미</option>
+                                                    <option value="A">의류</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label id="name">직거래 여부</label>
+                                                <input type="checkbox" class="form-control" id="check" v-model="item.isDirect"/>
+                                            </div>
+                                            <div class="form-group" v-if="item.isDirect == true">
+                                                <label id="name">직거래 장소</label>
+                                                <input type="text" class="form-control" id="name" v-model="item.dealRegion"/>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label id="price">가격</label>
+                                                <input type="number" class="form-control" id="price" v-model="item.price" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label id="description">상품 설명</label>
+                                                <textarea class="form-control" id="description" v-model="item.explanation" placeholder=""></textarea>
+                                            </div>
+                                            <!-- <div class="form-group">
+                                                <label id="privateKey">지갑 개인키</label>
+                                                <input id="privateKey" v-model="privateKey" type="text" class="form-control" placeholder="지갑 개인키를 입력해주세요." />
+                                            </div> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </v-flex>
+                </v-layout>
+                <v-layout row wrap>
+                    <v-flex>
+                        <button type="button" class="btn btn-primary" v-on:click="save" v-bind:disabled="isCreating">
+                            채팅
+                        </button>
+                    </v-flex>
+                    <v-flex>
+                        <button type="button" class="btn btn-primary" v-on:click="save" v-bind:disabled="isCreating">
+                            상품 등록
+                        </button>
+                    </v-flex>
+                    <v-flex>
+                        <button type="button" class="btn btn-primary" v-on:click="save" v-bind:disabled="isCreating">
+                            등록 취소
+                        </button>
+                    </v-flex>
+                </v-layout>
+            </div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import { create as createItem } from "@/api/item.js";
+import { registerItem } from "@/utils/itemInventory.js";
+
+export default {
+    name: "ItemCreate",
+    data() {
+        return {
+            item: {
+                name: "",
+                category: "",
+                explanation: "",
+                available: true,
+                seller: this.$store.state.user.id,
+                image: "",
+                isDirect: false,
+                dealRegion: "",
+                price: null,
+            },
+            privateKey: "",
+            userId: this.$store.state.user.id,
+            isCreating: false,
+        };
+    },
+    computed: {
+        /**
+         * public/images에 있는 로컬 이미지를 첨부해서 사용합니다.
+         * DB에는 이미지 파일 이름만 저장되고
+         * 화면에 보여줄 땐 'public/images/{파일이름}' 경로를 사용합니다.
+         */
+        imgLocalPath() {
+            if (this.item.imgName) {
+                return process.env.BASE_URL + "images/" + this.item.imgName;
+            }
+
+            return null;
+        },
+    },
+    methods: {
+        // 상품을 등록한다.
+        save() {
+            const vm = this;
+            this.isCreating = true; // 아이템 등록 중임을 화면에 표시, 등록이 끝나면 false로 변경
+            if (this.item.name.length <= 0 || this.item.category.length <= 0 || this.item.price === null || this.item.price <= 0 || this.item.image === null || this.item.image.length <= 0) {
+                alert("입력폼을 모두 입력해주세요.");
+                this.isCreating = false;
+                return;
+            }
+
+            createItem( // 상품 등록 - 백앤드
+                this.item,
+                function(success) {
+                    alert("상품 등록 성공!");
+                },
+                function(error) {
+                    console.error(error);
+                }
+            );
+            /**
+             * TODO: PJTⅢ 과제3 Req.1-1 [상품 등록]
+             * DB에 상품 등록 후 반환 받은 id를 이용해서 이더리움에 상품을 등록
+             */
+        },
+        onFileChange(input) {
+            var target = input.target;
+            var files = input.target.files || input.dataTransfer.files;
+            if (!files.length) return;
+            
+            var reader = new FileReader();
+            reader.onload = (e) => {
+                this.item.image = e.target.result;
+            }
+            reader.readAsDataURL(target.files[0]);
+        },
+    },
+};
+</script>
+
+<style>
+.image-area {
+    border: 2px dashed #ced4da;
+    padding: 1rem;
+    position: relative;
+}
+
+.image-area::before {
+    content: "업로드 된 이미지";
+    font-weight: bold;
+    text-transform: uppercase;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 0.8rem;
+    z-index: 1;
+}
+
+.image-area img {
+    z-index: 2;
+    position: relative;
+}
+
+.profile {
+    display: flex;
+    justify-content: center;
+    /* min-width: 100px; */
+    size: 200px;
+}
+
+.backImg {
+    width: 80%;
+    margin: auto;
+    border-radius: 2rem;
+    background-size: 152vh;
+    padding: 20px;
+    box-shadow: 3px 3px 5px 1px #595959;
+}
+</style>
