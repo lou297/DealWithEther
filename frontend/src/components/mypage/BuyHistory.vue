c4be1bc98b97cf6e909fe25916e63400b221f86e
이정훈 <xorolx297@naver.com>
2020-09-28 23:50:38 +0900

    add buy history / in progress / S03P23A103-27

---
+++ frontend/src/components/mypage/BuyHistory.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+<script>
+export default {
+  data() {
+    return {
+      hasWallet: this.$store.state.user.walletAddress != null,
+      viewName: ""
+    };
+  },
+  mounted: function() {
+    this.viewName = this.$route.name;
+  }
+};
+</script>
+
+<style></style>
