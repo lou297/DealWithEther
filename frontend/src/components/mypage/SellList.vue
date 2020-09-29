d1771d134ce15522e215c05f046abb0e6aa6c783
rkdals213 <rkdals213@naver.com>
2020-09-29 18:05:09 +0900

    SellList 약간 수정

--- frontend/src/components/mypage/SellList.vue
+++ frontend/src/components/mypage/SellList.vue
@@ -5,7 +5,7 @@
             <h1 style="margin: 2rem 0.7rem 1rem 2rem; display: inline-block;">거래 결과</h1>
         </div>
         <v-card color="basil">
-            <div >
+            <div>
                 <v-flex xl1 lg1 md1 sm0 xs0><span></span></v-flex>
                 <v-card>
                     <v-list three-line xl10 lg10 md10 sm12 xs12>
@@ -17,9 +17,9 @@
                                 <v-list-item-avatar size="100px">
                                     <v-img :src="imgPath(item.itemId)"></v-img>
                                 </v-list-item-avatar>
-                            <sell-list-card :item="item"></sell-list-card>
+                                <sell-list-card :item="item"></sell-list-card>
                             </v-list-item>
-                            <v-col id="progress-container" >
+                            <v-col id="progress-container">
                                 <v-stepper alt-labels v-model="item.state">
                                     <v-stepper-header>
                                         <v-stepper-step step="1" :complete="item.state === 'I'">
@@ -40,9 +40,9 @@
                                     </v-stepper-header>
                                 </v-stepper>
                                 <span>
-                                    <v-btn color="success" :disabled="item.state !== 'P'" @click="estimate">배송시작</v-btn>
+                                    <v-btn color="success" :disabled="item.state !== 'P'" @click="send(index)">배송시작</v-btn>
                                     <v-btn color="primary" :disabled="item.state !== 'C'" @click="decidePurchase">평가하기</v-btn>
-                                    {{item.state}}
+                                    {{ item.state }}
                                 </span>
                             </v-col>
                         </template>
@@ -57,100 +57,101 @@
 
 <script>
 import HNav from "@/components/common/HNav copy";
-import { findMySalePurchases } from "@/api/purchase";
+import {findMySalePurchases} from "@/api/purchase";
 import * as purchaseService from "@/api/purchase.js";
 import * as walletService from "@/api/wallet.js";
-import { findById } from "@/api/item";
+import {findById} from "@/api/item";
 import SellListCard from "@/components/mypage/SellListCard";
 import PurchaseTxDetailVue from "../escrow/PurchaseTxDetail.vue";
 
 export default {
-  components: {
-    SellListCard,
-    HNav,
-  },
-  data() {
-    return {
-      items: [],
-      userId: this.$store.state.user.id,
-      name: [],
-    };
-  },
-  computed: {
-    function() {
-      console.log(this.items.length);
-      for (let i = 0; i < this.items.length; i++) {
-        const temp = this.getItemName(this.items[i].itemId);
-        this.name.push(temp);
-        console.log(temp);
-      }
+    components: {
+        SellListCard,
+        HNav,
     },
-  },
-  methods: {
-    imgPath(id) {
-      console.log(process.env.VUE_APP_BACKEND);
-      return process.env.VUE_APP_BACKEND + "api/items/images/" + id + "_1";
+    data() {
+        return {
+            items: [],
+            userId: this.$store.state.user.id,
+            name: [],
+        };
     },
-    getPurchaseHistory() {
-      findMySalePurchases(this.userId, (res) => {
-        if (res) {
-          console.log(res);
-          this.items = res.data;
-        } else {
-          alert("개인키 인증에 실패하였습니다.");
-        }
-      });
-    },
-    estimate() {
-      alert("estimate");
-      // 평가하기 버튼
-    },
-    decidePurchase() {
-      alert("decide");
-      // 구매 확정 버튼
-    },
-    canclePurchase() {
-      alert("cancle");
-      // 구매 취소 버튼
-    },
-    send(index) {
-      const privateKey = prompt("배송하시려면 개인키를 입력하세요.");
-      console.log(this.userId);
-      const vm = this;
-      walletService.isValidPrivateKey(this.userId, privateKey, (res) => {
-        if (res) {
-          purchaseService.send(
-            vm.items[index].purchaseId,
-            privateKey,
-            (res) => {
-              alert("성공!");
-            },
-            (error) => {
-              alert("실패!");
+    computed: {
+        function() {
+            console.log(this.items.length);
+            for (let i = 0; i < this.items.length; i++) {
+                const temp = this.getItemName(this.items[i].itemId);
+                this.name.push(temp);
+                console.log(temp);
             }
-          );
-        } else {
-          alert("개인키 인증에 실패하였습니다.");
-          this.isCashCharging = false;
-        }
-      });
+        },
     },
-  },
-  mounted: function() {},
-  created() {
-    this.getPurchaseHistory();
-    for (let i = 0; i < this.items.length; i++) {
-      const temp = this.getItemName(this.items[i].itemId);
-      this.name.push(temp);
-      console.log(temp);
-    }
-  },
+    methods: {
+        imgPath(id) {
+            console.log(process.env.VUE_APP_BACKEND);
+            return process.env.VUE_APP_BACKEND + "api/items/images/" + id + "_1";
+        },
+        getPurchaseHistory() {
+            findMySalePurchases(this.userId, (res) => {
+                if (res) {
+                    console.log(res);
+                    this.items = res.data;
+                } else {
+                    alert("개인키 인증에 실패하였습니다.");
+                }
+            });
+        },
+        estimate() {
+            alert("estimate");
+            // 평가하기 버튼
+        },
+        decidePurchase() {
+            alert("decide");
+            // 구매 확정 버튼
+        },
+        canclePurchase() {
+            alert("cancle");
+            // 구매 취소 버튼
+        },
+        send(index) {
+            const privateKey = prompt("배송하시려면 개인키를 입력하세요.");
+            console.log(this.userId);
+            const vm = this;
+            walletService.isValidPrivateKey(this.userId, privateKey, (res) => {
+                if (res) {
+                    purchaseService.send(
+                        vm.items[index].purchaseId,
+                        privateKey,
+                        (res) => {
+                            alert("성공!");
+                        },
+                        (error) => {
+                            alert("실패!");
+                        }
+                    );
+                } else {
+                    alert("개인키 인증에 실패하였습니다.");
+                    this.isCashCharging = false;
+                }
+            });
+        },
+    },
+    mounted: function () {
+    },
+    created() {
+        this.getPurchaseHistory();
+        for (let i = 0; i < this.items.length; i++) {
+            const temp = this.getItemName(this.items[i].itemId);
+            this.name.push(temp);
+            console.log(temp);
+        }
+    },
 };
 </script>
 
 <style>
 ul {
-  display: flex;
-  list-style: none;
+    display: flex;
+    list-style: none;
 }
 </style>
