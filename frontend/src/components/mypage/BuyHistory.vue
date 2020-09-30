94236442601a51ba2f1d282e28ea1d202bfc1f85
이정훈 <xorolx297@naver.com>
2020-09-30 18:03:07 +0900

    add item info in purchase list / done / S03P23A103-36

--- frontend/src/components/mypage/BuyHistory.vue
+++ frontend/src/components/mypage/BuyHistory.vue
@@ -1,16 +1,16 @@
 <template>
   <v-row id="container">
-    <v-col cols="4">
+    <v-col cols="2" id="item-info-container">
       <v-img
-        :src="`https://picsum.photos/500/300?random`"
+        :src="imgPath(item.itemId)"
         :lazy-src="`https://picsum.photos/10/6?random`"
         aspect-ratio="1"
         @click="onClickItem()"
       >
       </v-img>
-      <p>{{ buyPurchase.itemName }}</p>
+      <p style="margin:0">{{ item.name }}</p>
     </v-col>
-    <v-col cols="8" id="progress-container">
+    <v-col cols="10" id="progress-container">
       <v-stepper alt-labels v-model="state">
         <v-stepper-header>
           <v-stepper-step step="1" :complete="state >= 1">
@@ -30,14 +30,14 @@
           </v-stepper-step>
         </v-stepper-header>
       </v-stepper>
-      <div>
-        <v-btn color="success" :disabled="state != 4">평가하기</v-btn>
-        <v-btn color="primary" :disabled="state != 3" @click="confirm"
-          >구매 확정</v-btn
-        >
+      <div id="btn-container">
         <v-btn color="error" :disabled="state > 2" @click="cancel"
           >구매취소</v-btn
         >
+        <v-btn color="primary" :disabled="state != 3" @click="confirm"
+          >구매 확정</v-btn
+        >
+        <v-btn color="success" :disabled="state != 4">평가하기</v-btn>
       </div>
     </v-col>
   </v-row>
@@ -45,35 +45,54 @@
 <script>
 import * as purchaseService from "@/api/purchase.js";
 import * as walletService from "@/api/wallet.js";
+import * as itemService from "@/api/item.js";
 export default {
   props: ["buyPurchase"],
   data() {
     return {
       userId: this.$store.state.user.id,
       state: -1,
+      item: {},
     };
   },
   created() {
-    switch (this.buyPurchase.state) {
-      case "I":
-        this.state = 1;
-        break;
-      case "P":
-        this.state = 2;
-        break;
-      case "S":
-        this.state = 3;
-        break;
-      case "C":
-        this.state = 4;
-        break;
-      case "X":
-        this.state = 5;
-        break;
-    }
+    this.updateState()
+    this.fetchPurchaseInfo()
   },
   mounted: function() {},
   methods: {
+    updateState() {
+      switch (this.buyPurchase.state) {
+        case "I":
+          this.state = 1;
+          break;
+        case "P":
+          this.state = 2;
+          break;
+        case "S":
+          this.state = 3;
+          break;
+        case "C":
+          this.state = 4;
+          break;
+        case "X":
+          this.state = 5;
+          break;
+      }
+    },
+    imgPath(id) {
+      return process.env.VUE_APP_BACKEND + "api/items/images/" + id + "_1";
+    },
+    fetchPurchaseInfo() {
+      itemService.findById(this.buyPurchase.itemId,
+        res => {
+          this.item = res.data
+        },
+        err => {
+          alert(err)
+        }
+      )
+    },
     onClickItem() {
       alert("클릭");
       this.$router.push("../item/detail/" + this.buyPurchase.itemId);
@@ -129,7 +148,15 @@ export default {
   border: 1px solid black;
 }
 
+#item-info-container{
+  align-self: center;
+}
+
 #progress-container {
   align-self: center;
+}
+
+#btn-container {
+  float: right;
 }
 </style>
