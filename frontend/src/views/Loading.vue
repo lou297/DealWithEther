9334406ca48cf609d291117d0bec82e120ef152a
rkdals213 <rkdals213@naver.com>
2020-10-06 15:36:13 +0900

    로더 추가

---
+++ frontend/src/views/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <cube-spin></cube-spin>
+    </div>
+</template>
+
+<script>
+import CubeSpin from 'vue-loading-spinner'
+
+export default {
+    components: {
+        CubeSpin
+    }
+}
+</script>
